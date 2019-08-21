d9d1d9586c481886ee327ddcd5c74a0c2b4f2541
cao xuan my <cao.bada96@gmail.com>
2019-08-21 18:38:56 +0700

    init Source view Laravel

---
+++ resources/views/wrapper.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AdminLTE 2 | Dashboard</title>
+  @include('admin.wraplib.wrapper_css')
+</head>
+<body class="hold-transition skin-blue sidebar-mini">
+<div class="wrapper">
+
+ @include('admin.wrapper.header')
+ @include('admin.wrapper.sidebar')
+
+     <div class="content-wrapper">
+    
+    </div>
+  
+  <!-- /.control-sidebar -->
+  <!-- Add the sidebar's background. This div must be placed
+       immediately after the control sidebar -->
+  <!-- <div class="control-sidebar-bg"></div> -->
+</div>
+<!-- ./wrapper -->
+
+ @include('admin.wraplib.wrapper_js')
+</body>
+</html>
